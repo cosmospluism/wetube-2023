6688dd328893645f23612e5d0c9aaeac6fc11796
0:) <v-lvkdl@naver.com>
2024-02-22 18:01:01 +0900

    240222 thumbnail part

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,6 +1,7 @@
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
-const startBtn = document.getElementById("startBtn");
-const startBtnIcon = startBtn.querySelector("i");
+const actionBtn = document.getElementById("actionBtn");
+const actionBtnIcon = actionBtn.querySelector("i");
+const actionBtnSpan = actionBtn.querySelector("span");
 const videoPreview = document.getElementById("preview");
 const downloadBtn = document.getElementById("downloadBtn");
 
@@ -8,28 +9,47 @@ let stream;
 let recorder;
 let videoFile;
 
+const files = {
+  input: "recording.webm",
+  output: "output.mp4",
+  thumb: "thumbnail.jpg",
+};
+
+const downloadFunc = function (fileUrl, fileName) {
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+};
+
 const handleDownload = async () => {
+  actionBtn.removeEventListener("click", handleStart);
+  actionBtnIcon.className = "";
+  actionBtnSpan.innerText = "Transcoding...";
+  actionBtn.disabled = true;
+
   const ffmpeg = createFFmpeg({ log: true });
   await ffmpeg.load(); //프로그램이라 로드할 때 시간이 걸리기 떄문에 await 해줌
 
   // ffmpeg 프로그램의 FS(파일 시스템) 내 메모리에
   // 비디오 파일을 가져와 recording.webm이름으로 파일 생성(writeFile)
-  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
   // 생성 파일 webm -> mp4 포맷 변환
-  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  await ffmpeg.run("-i", files.input, "-r", "60", files.output);
   // 생성 파일 webm -> jpg 썸네일 이미지 추출
   await ffmpeg.run(
     "-i",
-    "recording.webm",
+    files.input,
     "-ss",
     "00:00:01",
     "-frames:v",
     "1",
-    "thumbnail.jpg"
+    files.thumb
   );
   // 포맷변환해 생성한 파일 불러오기
-  const mp4File = ffmpeg.FS("readFile", "output.mp4");
-  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+  const mp4File = ffmpeg.FS("readFile", files.output);
+  const thumbFile = ffmpeg.FS("readFile", files.thumb);
 
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
   const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
@@ -37,34 +57,31 @@ const handleDownload = async () => {
   const mp4Url = URL.createObjectURL(mp4Blob);
   const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  const a = document.createElement("a");
-  a.href = mp4Url;
-  a.download = "MyRecording.mp4";
-  document.body.appendChild(a);
-  a.click();
+  downloadFunc(mp4Url, "MyRecording.mp4");
   videoPreview.pause();
-
-  const thumbA = document.createElement("a");
-  thumbA.href = thumbUrl;
-  thumbA.download = "MyThumbnail.jpg";
-  document.body.appendChild(thumbA);
-  thumbA.click();
+  downloadFunc(thumbUrl, "MyThumbnail.jpg");
 
   // unlink = delete a file
-  ffmpeg.FS("unlink", "recording.webm");
-  ffmpeg.FS("unlink", "output.mp4");
-  ffmpeg.FS("unlink", "thumbnail.jpg");
+  ffmpeg.FS("unlink", files.input);
+  ffmpeg.FS("unlink", files.output);
+  ffmpeg.FS("unlink", files.thumb);
 
   // createObjectURL()을 통해 생성한 url해제(메모리 차지 없앰)
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(thumbUrl);
   URL.revokeObjectURL(videoFile);
+
+  actionBtn.disabled = false;
+  actionBtnIcon.className = "fas fa-circle";
+  actionBtnSpan.innerText = "Recording again";
+  actionBtn.addEventListener("click", handleStart); // 수정 필요 24.02.22
 };
 
 const handleStop = () => {
-  startBtnIcon.className = "fas fa-circle";
-  startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
+  actionBtnIcon.className = "fas fa-arrow-down";
+  actionBtnSpan.innerText = "Download Video";
+  actionBtn.removeEventListener("click", handleStop);
+  actionBtn.addEventListener("click", handleDownload);
 
   recorder.stop();
   // 녹화를 종료하면 dataavailable 이벤트 발생
@@ -80,9 +97,10 @@ const handleStop = () => {
 };
 
 const handleStart = () => {
-  startBtnIcon.className = "fas fa-stop";
-  startBtn.removeEventListener("click", handleStart);
-  startBtn.addEventListener("click", handleStop);
+  actionBtnIcon.className = "fas fa-stop";
+  actionBtnSpan.innerText = "Stop Recording";
+  actionBtn.removeEventListener("click", handleStart);
+  actionBtn.addEventListener("click", handleStop);
 
   recorder = new window.MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.start();
@@ -98,5 +116,4 @@ const init = async () => {
 };
 init();
 
-startBtn.addEventListener("click", handleStart);
-downloadBtn.addEventListener("click", handleDownload);
+actionBtn.addEventListener("click", handleStart);
